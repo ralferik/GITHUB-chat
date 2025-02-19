176e2d57cf23b2e1e8749c28da58b8235a5476dd
Karl Kaspar Leedo <kleedo@taltech.ee>
2025-02-19 20:20:43 +0200

    FIXes

--- Casino1.0/Assets/Scripts/PlayerMovement.cs
+++ Casino1.0/Assets/Scripts/PlayerMovement.cs
@@ -18,29 +18,24 @@ public class PlayerMovement : MonoBehaviour
  
     bool isGrounded;
  
-    // Update is called once per frame
     void Update()
     {
-        //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
  
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
- 
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
  
-        //right is the red Axis, foward is the blue axis
         Vector3 move = transform.right * x + transform.forward * z;
  
         controller.Move(move * speed * Time.deltaTime);
  
-        //check if the player is on the ground so he can jump
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            //the equation for jumping
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
  
